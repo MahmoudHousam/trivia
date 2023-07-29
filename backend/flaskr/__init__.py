170faa99666ce00e0d0b37f7d856c7e04d4dbfc1
MahmoudHousam <mahmoudhousam60@gmail.com>
2023-07-30 01:58:47 +0300

    edit CORS

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -30,9 +30,6 @@ def create_app(test_config=None):
     def after_request(response):
         response.headers.add('Access-Control-Allow-Headers',
                              'Content-Type,Authorization,true')
-        response.headers.add('Access-Control-Allow-Origin',
-                             'http://localhost:3000')
-        response.header.add('Access-Control-Allow-Credentials', 'true')
         response.headers.add('Access-Control-Allow-Methods',
                              'GET,PUT,POST,DELETE,OPTIONS')
         return response
